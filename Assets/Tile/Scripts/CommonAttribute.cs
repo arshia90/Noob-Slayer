97a9bdcc4f552d648695ff5c0627e058e0d74b9c
arshia <241413124+arshia90@users.noreply.github.com>
2026-05-06 11:37:02 +0330

    Creating CommonAttribute script and Character scripts

---
+++ Assets/Tile/Scripts/CommonAttribute.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public  class CommonAttribute : MonoBehaviour
+{
+    public int Hp;
+
+    public int Mana;
+
+    public int Armor;
+
+    public int ActionPoints;
+
+    public int Xp;
+ 
+   
+}
